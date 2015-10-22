5c44cbc0cb0c48f9391f5ad6f6e91da9c4f06a55
Ralph Hempel <rhempel@hempeldesigngroup.com>
2015-10-21 21:24:16 -0400

    Added all authors

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='python-ev3dev',
     version='0.0.1',
     description='Python language bindings for ev3dev',
-    author='Ralph Hempel',
+    author='Ralph Hempel/Denis Demidov/Anton Vanhoucke',
     license='MIT',
     url='https://github.com/rhempel/ev3dev-lang-python',
     include_package_data=True,
