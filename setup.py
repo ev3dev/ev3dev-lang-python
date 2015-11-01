c5fce671a27aec21427f74704b25750857d8bfa8
Ralph Hempel <rhempel@hempeldesigngroup.com>
2015-11-01 17:06:04 -0500

    Minor changes to Debian packaging files

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     name='python-ev3dev',
     version=git_version(),
     description='Python language bindings for ev3dev',
-    author='Ralph Hempel et al - See ev3dev.py for credits',
+    author='Ralph Hempel'
     author_email='rhempel@hempeldesigngroup.com',
     license='MIT',
     url='https://github.com/rhempel/ev3dev-lang-python',
