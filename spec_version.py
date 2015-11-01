aed4c22faff1f4b2db13c6717ca8fe97db9448ac
Denis Demidov <dennis.demidov@gmail.com>
2015-11-01 14:49:49 +0300

    Fix spec_version generation
    See ev3dev/ev3dev-lang#114

--- spec_version.py
+++ spec_version.py
@@ -1,4 +1,4 @@
-#~autogen ../python/templates/python_spec_version
-spec_version = "spec: 0.9.3-pre-r1, kernel: v3.16.7-ckt10-4-ev3dev-ev3"
+#~autogen spec_version
+spec_version = "spec: 0.9.3-pre-r2, kernel: v3.16.7-ckt16-7-ev3dev-ev3"
 
 #~autogen
