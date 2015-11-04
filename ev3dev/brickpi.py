ce53556983b66c24df2799b6fdcd17ca68d5156f
Anton Vanhoucke <Anton@fabrique.nl>
2015-11-04 22:16:53 +0100

    BrickPi Powersupply

--- ev3dev/brickpi.py
+++ ev3dev/brickpi.py
@@ -27,6 +27,7 @@ An assortment of classes modeling specific features of the BrickPi.
 """
 
 from .core import *
+import smbus
 
 
 OUTPUT_A = 'ttyAMA0:outA'
@@ -70,3 +71,45 @@ class Leds(object):
 
 
 # ~autogen
+
+
+class PowerSupply(object):
+
+    @property
+    def measured_voltage(self):
+        """
+        The measured voltage that the battery is supplying (in microvolts)
+
+        Reads the digital output code of the MCP3021 chip on the BrickPi+ over i2c.
+        Some bit operation magic to get a voltage floating number.
+
+        If this doesnt work try this on the command line: i2cdetect -y 1
+        The 1 in there is the bus number, same as in bus = smbus.SMBus(1)
+        Google the resulting error.
+
+        :return int: voltage in microvolts
+        """
+
+        try:
+                bus = smbus.SMBus(1)            # SMBUS 1 because we're using greater than V1.
+                address = 0x48
+                # time.sleep(0.1) #Is this necessary?
+
+                # read data from i2c bus. the 0 command is mandatory for the protocol but not used in this chip.
+                data = bus.read_word_data(address, 0)
+
+                # from this data we need the last 4 bites and the first 6.
+                last_4 = data & 0b1111 # using a byte mask
+                first_6 = data >> 10 # left shift 10 because data is 16 bits
+
+                # together they make the voltage conversion ratio
+                # to make it all easier the last_4 bits are most significant :S
+                vdata = ((last_4 << 6) | first_6)
+
+                # Now we can calculate the battery voltage like so:
+                voltage = vdata * 0.0179 * 1000000    # This is an empirical number for voltage conversion.
+
+                return voltage
+
+        except:
+                return 0
