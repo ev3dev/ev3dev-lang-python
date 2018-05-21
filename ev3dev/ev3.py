2e4ae0af4801b3691d012c537e565bd50b961b49
Kaelin Laundry <wasabifan@outlook.com>
2018-05-20 22:20:49 -0700

    Remove Stretch-only changes accidentally introduced to Jessie branch

--- ev3dev/ev3.py
+++ ev3dev/ev3.py
@@ -47,10 +47,10 @@ class Leds(object):
 
 # ~autogen led-colors platforms.ev3.led>currentClass
 
-    red_left = Led(name_pattern='led0:red:brick-status')
-    red_right = Led(name_pattern='led1:red:brick-status')
-    green_left = Led(name_pattern='led0:green:brick-status')
-    green_right = Led(name_pattern='led1:green:brick-status')
+    red_left = Led(name_pattern='ev3:left:red:ev3dev')
+    red_right = Led(name_pattern='ev3:right:red:ev3dev')
+    green_left = Led(name_pattern='ev3:left:green:ev3dev')
+    green_right = Led(name_pattern='ev3:right:green:ev3dev')
 
     LEFT = ( red_left, green_left, )
     RIGHT = ( red_right, green_right, )
@@ -165,12 +165,12 @@ class Button(ButtonEVIO):
 
 
     _buttons = {
-            'up': {'name': '/dev/input/by-path/platform-gpio_keys-event', 'value': 103},
-            'down': {'name': '/dev/input/by-path/platform-gpio_keys-event', 'value': 108},
-            'left': {'name': '/dev/input/by-path/platform-gpio_keys-event', 'value': 105},
-            'right': {'name': '/dev/input/by-path/platform-gpio_keys-event', 'value': 106},
-            'enter': {'name': '/dev/input/by-path/platform-gpio_keys-event', 'value': 28},
-            'backspace': {'name': '/dev/input/by-path/platform-gpio_keys-event', 'value': 14},
+            'up': {'name': '/dev/input/by-path/platform-gpio-keys.0-event', 'value': 103},
+            'down': {'name': '/dev/input/by-path/platform-gpio-keys.0-event', 'value': 108},
+            'left': {'name': '/dev/input/by-path/platform-gpio-keys.0-event', 'value': 105},
+            'right': {'name': '/dev/input/by-path/platform-gpio-keys.0-event', 'value': 106},
+            'enter': {'name': '/dev/input/by-path/platform-gpio-keys.0-event', 'value': 28},
+            'backspace': {'name': '/dev/input/by-path/platform-gpio-keys.0-event', 'value': 14},
         }
 
     @property
