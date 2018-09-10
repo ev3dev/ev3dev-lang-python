3ff239d96b3b2da85faf637c2dc1fb85a9ba2aa2
Kaelin Laundry <wasabifan@outlook.com>
2018-09-09 21:04:14 -0700

    install upip and dependencies

--- .travis/install-micropython.sh
+++ .travis/install-micropython.sh
@@ -1,7 +1,18 @@
 #!/bin/sh
 set -ex
+
 cd ~/
-git clone --recurse-submodules https://github.com/micropython/micropython.git --depth 1
+
+# Build micropython from source
+# TODO: cache build output
+git clone --recurse-submodules https://github.com/micropython/micropython.git --depth 1 --branch v1.9.4
 cd ./micropython/ports/unix
 make axtls
 make
+
+# Install upip
+~/micropython/tools/bootstrap_upip.sh
+
+# Install dependencies
+# TODO: make unittest show output
+~/micropython/ports/unix/micropython -m upip install micropython-unittest micropython-os micropython-os.path micropython-shutil micropython-io micropython-fnmatch micropython-numbers micropython-struct micropython-time micropython-logging micropython-select
