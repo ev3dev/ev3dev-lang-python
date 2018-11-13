f4905cf63a7f61ee1c9425b4a52ddc5ee332f4d2
Daniel Walton <dwalton76@gmail.com>
2018-11-13 19:29:02 +0000

    Add port name constants for stacked brickpi3s

--- ev3dev2/_platform/brickpi3.py
+++ ev3dev2/_platform/brickpi3.py
@@ -21,10 +21,25 @@ OUTPUT_N = 'spi0.1:MN'
 OUTPUT_O = 'spi0.1:MO'
 OUTPUT_P = 'spi0.1:MP'
 
-INPUT_1 = 'spi0.1:S1' 
-INPUT_2 = 'spi0.1:S2' 
-INPUT_3 = 'spi0.1:S3' 
+INPUT_1 = 'spi0.1:S1'
+INPUT_2 = 'spi0.1:S2'
+INPUT_3 = 'spi0.1:S3'
 INPUT_4 = 'spi0.1:S4'
+
+INPUT_5 = 'spi0.1:S5'
+INPUT_6 = 'spi0.1:S6'
+INPUT_7 = 'spi0.1:S7'
+INPUT_8 = 'spi0.1:S8'
+
+INPUT_9 = 'spi0.1:S9'
+INPUT_10 = 'spi0.1:S10'
+INPUT_11 = 'spi0.1:S11'
+INPUT_12 = 'spi0.1:S12'
+
+INPUT_13 = 'spi0.1:S13'
+INPUT_14 = 'spi0.1:S14'
+INPUT_15 = 'spi0.1:S15'
+INPUT_16 = 'spi0.1:S16'
 
 BUTTONS_FILENAME = None
 EVDEV_DEVICE_NAME = None
