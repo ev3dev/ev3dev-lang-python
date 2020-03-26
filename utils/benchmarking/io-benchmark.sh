a009348cf365744f0a422959a078f2c148cadc8b
Kaelin Laundry <wasabifan@outlook.com>
2020-03-25 22:33:28 -0700

    WIP benchmarking script

---
+++ utils/benchmarking/io-benchmark.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+exec &> >(tee benchmark.txt)
+
+do_benchmark() {
+    PYTHON=$1
+
+    VERSION=$($PYTHON -c "import ev3dev2; print(ev3dev2.__version__)")
+    MODULE_PATH=$($PYTHON -c "import ev3dev2; print(ev3dev2.__path__)")
+
+    echo "Using interpreter $PYTHON; found ev3dev2 version $VERSION from $MODULE_PATH"
+
+    echo "Motor read address:"
+    $PYTHON -m timeit -s "from ev3dev2.motor import Motor; m = Motor()" "m.address"
+    
+    echo "Motor read speed_sp:"
+    $PYTHON -m timeit -s "from ev3dev2.motor import Motor; m = Motor()" "m.speed_sp"
+    
+    echo "Motor write speed_sp:"
+    $PYTHON -m timeit -s "from ev3dev2.motor import Motor; m = Motor()" "m.speed_sp = 5"
+}
+
+do_benchmark python3
+do_benchmark micropython
